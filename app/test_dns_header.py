feb85c40baa0b6b138902b06f6da5aafb5fc33e0
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-10-21 15:44:37 -0600

    VSCode: Clean up errors

--- app/test_dns_header.py
+++ app/test_dns_header.py
@@ -1,17 +1,16 @@
-from app.protocol.dns_header import DNSHeader, QueryResponseValue
-
+from app.protocol.dns_header import DNSHeader, QueryResponseValue, QueryOpcode, ResponseOpcode
 def test_building_a_header():
 
     header = DNSHeader(
         identifier=1234,
         query_response_indicator=QueryResponseValue.Reply,
-        operation_code=0,
+        operation_code=QueryOpcode.Query,
         authoritative_answer=True,
         truncation=False,
         recursion_desired=False,
         recursion_available=False,
         reserved=0,
-        response_code=0,
+        response_code=ResponseOpcode.NoError,
         question_count=0,
         answer_count=0,
         authority_record_count=0,
